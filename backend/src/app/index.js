cfc10fab61e72fe5e5aabfdd8e41261b76b577b2
Nick <keller108@yandex.ru>
2023-04-30 12:48:03 +0500

    Добавлено шифрование пароля bcrypt

--- backend/src/app/index.js
+++ backend/src/app/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import { validationResult } from 'express-validator';
 import { registerValidation } from '../validations/auth.js';
+import UserModel from "../entities/User";
 
 const app = express();
 
@@ -17,12 +19,25 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.post("/auth/register", registerValidation, (req, res) => {
+app.post("/auth/register", registerValidation, async (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
         return res.status(400).json(errors.array());
     }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        password: passwordHash,
+    });
+
+    const user = await doc.save();
 
     res.json({
         success: true,
